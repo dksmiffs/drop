307d0e462c6901fb62b507c6e173502946a85e25
Dave Smith <dave.k.smith@gmail.com>
2018-05-06 20:49:14 -0500

    as generated by libGDX setup

---
+++ desktop/src/com/mygdx/third/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.third.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.third.MyGdxThird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxThird(), config);
+	}
+}
