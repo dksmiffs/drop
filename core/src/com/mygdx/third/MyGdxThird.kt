84f125b72030b536637863a3bfa9b7f53e3f4840
Dave Smith <dave.k.smith@gmail.com>
2018-05-07 23:03:24 -0500

    beginnings of a Kotlin replacement for main source file

---
+++ core/src/com/mygdx/third/MyGdxThird.kt
@@ -0,0 +1,17 @@
+package com.mygdx.third
+
+import com.badlogic.gdx.ApplicationAdapter
+import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+class MyGdxThird : ApplicationAdapter() {
+  var batch = SpriteBatch()
+  var img = Texture("badlogic.jpg")
+
+  override fun render() {
+  }
+
+}
+
