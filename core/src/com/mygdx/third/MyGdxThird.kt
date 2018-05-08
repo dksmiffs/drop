96510809b71a10879fd329e4caaf21df7c127293
Dave Smith <dave.k.smith@gmail.com>
2018-05-07 23:35:08 -0500

    got Kotlin replacement file working, removed Java source

--- core/src/com/mygdx/third/MyGdxThird.kt
+++ core/src/com/mygdx/third/MyGdxThird.kt
@@ -7,10 +7,25 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 class MyGdxThird : ApplicationAdapter() {
-  var batch = SpriteBatch()
-  var img = Texture("badlogic.jpg")
+  private lateinit var batch: SpriteBatch
+  private lateinit var img: Texture
+
+  override fun create() {
+    batch = SpriteBatch()
+    img = Texture("badlogic.jpg")
+  }
 
   override fun render() {
+    Gdx.gl.glClearColor(1f, 0f, 0f, 1f)
+    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
+    batch.begin()
+    batch.draw(img, 0f, 0f)
+    batch.end()
+  }
+
+  override fun dispose() {
+    batch.dispose()
+    img.dispose()
   }
 
 }
