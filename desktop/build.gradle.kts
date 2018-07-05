719fc921109bdf7006115d11e4deeb451abcbb72
Dave Smith <dave.k.smith@gmail.com>
2018-07-04 23:41:32 -0500

    Functions in Kotlin DSL, but still haven't figured how to ignoreExitValue

--- desktop/build.gradle.kts
+++ desktop/build.gradle.kts
@@ -3,38 +3,11 @@
 // java.srcDirs is a property of the source set specifying the source directory
 //    containing Java source.
 //
-// Proposed alternate Kotlin DSL syntax:
-//    java {
-//      val mainSet: SourceDirectorySet = sourceSets.getByName("main").java
-//      mainSet.srcDir("src/")
-//    }
-sourceSets.main.java.srcDirs = [ "src/" ]
+// Original Groovy DSL syntax:
+//    sourceSets.main.java.srcDirs = [ "src/" ]
+val mainSet: SourceDirectorySet = java.sourceSets.getByName("main").java
+mainSet.srcDir("src/")
 
-// From stackoverflow.com/a/21700818 (author Peter Niederwieser):
-//    ext is shorthand for project.ext, and is used to define *extra properties*
-//    for the project object. (It's also possible to define extra properties for
-//    many other objects.) When reading an extra property, the ext. is omitted
-//    (e.g. println project.springVersion or println springVersion). The same
-//    works from within methods. It does not make sense to declare a method
-//    named ext.
-// Official Gradle documentation for extra properties (under docs.gradle.org):
-//    current/userguide/writing_build_scripts.html#sec:extra_properties
-// kotlin-dsl samples which are documented to be "equivalent of the ext
-//    properties found in Gradle Script Groovy":
-//       github.com/gradle/kotlin-dsl/tree/master/samples/extra-properties
-// Official Gradle documentation on a Project:
-//    docs.gradle.org/current/javadoc/org/gradle/api/Project.html
-// Kotlin DSL alternative documented in the following two locations:
-//    github.com/gradle/kotlin-dsl/issues/626
-//    github.com/gradle/kotlin-dsl/releases
-//      * scroll to "Updates since v0.15.6"
-//      * scroll more to "Unified DSL for Gradle/Project and extra properties"
-//
-// Proposed alternate Kotlin DSL syntax:
-//    val mainClassName: String by project = "drop.DesktopLauncher"
-//    val assetsDir: File by project = File("../core/assets")
-project.ext.mainClassName = "drop.DesktopLauncher"
-project.ext.assetsDir = new File("../core/assets");
 
 // This guide provided some help for migrating a task from Groovy to Kotlin DSL:
 //    github.com/jnizet/gradle-kotlin-dsl-migration-guide#creating-a-task
@@ -42,20 +15,22 @@ project.ext.assetsDir = new File("../core/assets");
 //    current/userguide/more_about_tasks.html#sec:adding_dependencies_to_tasks
 // Example of "dependsOn" usage in Kotlin DSL:
 //    github.com/gradle/kotlin-dsl-docs/blob/master/build.gradle.kts
-// Don't know what the "classes" task is, but it was working in Gradle Groovy
+// Source that finally provided insight on setting classpath:
+//    github.com/JLLeitschuh/pmd-kotlin/blob/master/build.gradle.kts
 //
-// Proposed alternate Kotlin DSL syntax:
-//    task run by tasks.creating(JavaExec::class) {
-//      main = project.mainClassName
-//      classpath = java.mainSet.runtimeClasspath
-//      workingDir = project.assetsDir
-//      ignoreExitValue = true
-//      dependsOn(classes)
+// Original Groovy DSL syntax:
+//    task run(dependsOn: classes, type: JavaExec) {
+//        main = project.mainClassName
+//        classpath = sourceSets.main.runtimeClasspath
+//        workingDir = project.assetsDir
+//        ignoreExitValue = true
 //    }
-task run(dependsOn: classes, type: JavaExec) {
-    main = project.mainClassName
-    classpath = sourceSets.main.runtimeClasspath
-    workingDir = project.assetsDir
-    ignoreExitValue = true
+tasks {
+  val run by creating(JavaExec::class) {
+    main = "drop.DesktopLauncher"
+    classpath = java.sourceSets["main"].runtimeClasspath
+    workingDir = File("../core/assets")
+//    ignoreExitValue = true
+  }
 }
 
